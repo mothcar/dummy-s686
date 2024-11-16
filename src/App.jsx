57a7444db62e28a70d2096082fb3e48e364e6b3b
Paul <mothcar@gmail.com>
2024-11-16 18:48:07 +0900

    three dots props

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react'
 import { Link } from "react-router-dom";
 import './App.css'
+import Child from './components/Child.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
+  // const ItemsList = [
+  //   {
+  //     id: 1,
+  //     name: "VA-11 Hall-A: Cyberpunk Bartender Action",
+  //     price: 110000,
+  //     image: "some image",
+  //     link: "https://store.steampowered.com/app/447530/VA11_HallA_Cyberpunk_Bartender_Action/?l=koreana",
+  //   },
+  // ];
   const ItemsList = [
-    {
-      id: 1,
-      name: "VA-11 Hall-A: Cyberpunk Bartender Action",
-      price: 110000,
-      image: "some image",
-      link: "https://store.steampowered.com/app/447530/VA11_HallA_Cyberpunk_Bartender_Action/?l=koreana",
-    },
+    "David", "Bella"
   ];
 
   return (
     <>
-      <div></div>
+      <div><Child toChild={ItemsList}/></div>
       <h1>Vite + React</h1>
       <div className="card">
         <Link to="/about" state={ItemsList} >Go About</Link>
